305a1fc7d8d3ee7303d6004d91e58116c583d318
Chris <thatguynamedchriszinser@gmail.com>
2019-05-09 15:34:44 -0700

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -7,18 +7,12 @@ $.post('https://node.c-development.com', {id_token:"key"})
   
   
       
-$.get(`https://sheets.googleapis.com/v4/spreadsheets/1Df2YX5DcUY1bWJRlxcona9RsL0iEVH_W9RM7P57KXwk/values/Sheet1!A1:D5?key=${user}`, function (response2) {
+$.get(`https://sheets.googleapis.com/v4/spreadsheets/1Df2YX5DcUY1bWJRlxcona9RsL0iEVH_W9RM7P57KXwk/values/Sheet1!A3:D?key=${user}`, function (response2) {
 //var source = $(`${response}`);
 //$('#files js-navigation-container js-active-navigation-container').html(source),function(responce2){
 console.log(response2)
 //}});
-  var cleanText = response2.replace(/<\/?[^>]+(>|$)/g, "");
-  var trimmed = cleanText.trim()
-  var prep1  = trimmed.split(" ")
-  var filtered = prep1.filter(function(value, index, arr){
-    return value.includes(".txt")
-})
-console.log("Filtered:" + filtered)
+
 }
 )
 
