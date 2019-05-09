08d68b522d70d864f5204c14ad2044b8e7858d41
Chris <thatguynamedchriszinser@gmail.com>
2019-05-09 15:24:43 -0700

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -10,7 +10,7 @@ $.post('https://node.c-development.com', {id_token:"key"})
 $.get(`https://sheets.googleapis.com/v4/spreadsheets/1Df2YX5DcUY1bWJRlxcona9RsL0iEVH_W9RM7P57KXwk/values/Sheet1!A1:D5?key=${user}`, function (response2) {
 //var source = $(`${response}`);
 //$('#files js-navigation-container js-active-navigation-container').html(source),function(responce2){
-console.log(responce2)
+console.log(response2)
 //}});
   var cleanText = response2.replace(/<\/?[^>]+(>|$)/g, "");
   var trimmed = cleanText.trim()
