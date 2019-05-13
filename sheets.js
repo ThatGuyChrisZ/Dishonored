960fc23555316b5beedca5cd962ae3027def4fec
Chris <thatguynamedchriszinser@gmail.com>
2019-05-13 12:41:43 -0700

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -21,7 +21,7 @@ console.log(response2)
 }
 )
         $.get(`https://sheets.googleapis.com/v4/spreadsheets/1Df2YX5DcUY1bWJRlxcona9RsL0iEVH_W9RM7P57KXwk/values/Sheet1!E3:E?key=${user}`, function (response3) {
-              console.log(response3.values[1] )
+              console.log(response3)
                 document.getElementById("mainbody").innerHTML+= response3.values[0] 
                  document.getElementById("announce").innerHTML+= response3.values[1] 
         });
