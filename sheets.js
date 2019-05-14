56a3dbb89420fc9a1b43a44424ec626cea18f09f
Chris <thatguynamedchriszinser@gmail.com>
2019-05-13 17:36:28 -0700

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -36,7 +36,7 @@ console.log(response2)
                       
                     
                  document.getElementById("announce").style.textIndent = inlined.toString() + "em"
-                       if(inlined  < initial * -1){
+                       if(inlined  < (initial * -1) -20){
                        inlined = initial
                        }
 },10)
